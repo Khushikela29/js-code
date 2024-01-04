fc917487e6f2ca5265b8d98a7c4aed5036f99dc8
Khushikela29 <Khushikela29@gmail.com>
2024-01-04 10:33:23 +0530

    NumberFile

---
+++ 01_basics/numbers.js
@@ -0,0 +1,11 @@
+const score= 400
+console.log(score);
+
+const balance= new Number(300)
+console.log(balance)
+console.log(balance.toString().length)
+console.log(balance.toFixed(1))
+
+// round off values
+const othernum= 469.5697
+console.log(othernum.toPrecision(4));
